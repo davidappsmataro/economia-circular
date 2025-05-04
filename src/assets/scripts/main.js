3b10dfdffd99c986a52d39f449de427bdfc46028
David <david@grupcm.es>
2025-05-04 20:45:18 +0200

    Mejorando codigo

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -2,7 +2,7 @@
  * Import dependencies from node_modules
  * see commented examples below
  */
-import * as bootstrap from 'bootstrap';
+import * as bootstrap from "bootstrap";
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
@@ -10,7 +10,22 @@ import * as bootstrap from 'bootstrap';
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
++(function () {
+  const bots = bootstrap;
+  /* const university = "UOC";
+  console.log(`Hello, ${university}!`); */
+
+  // Añadir la clase 'active' al enlace del navbar correspondiente a la ruta actual
+  const currentPath = window.location.pathname.split("/").pop();
+
+  const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+
+  navLinks.forEach((link) => {
+    const href = link.getAttribute("href");
+    if (href === currentPath) {
+      link.classList.add("active"); // Añade la clase 'active'
+    } else {
+      link.classList.remove("active"); // Quita la clase de otros enlaces
+    }
+  });
+})();
