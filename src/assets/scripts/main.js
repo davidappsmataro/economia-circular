3dc81a2b833e6e45a42a59e4f2afdc4ee4cf8b1e
David <david@grupcm.es>
2025-05-08 16:34:55 +0200

    Instalado Leafleft para el mapa de contacto y mejoras codigo

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -3,6 +3,12 @@
  * see commented examples below
  */
 import * as bootstrap from "bootstrap";
+import L from "leaflet";
+//import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerIcon from "../images/marcador.png";
+
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import "leaflet/dist/leaflet.css";
 // import 'some-node-module';
 // import SomeModule from 'some-node-module';
 
@@ -12,8 +18,6 @@ import * as bootstrap from "bootstrap";
 
 +(function () {
   const bots = bootstrap;
-  /* const university = "UOC";
-  console.log(`Hello, ${university}!`); */
 
   // Añadir la clase 'active' al enlace del navbar correspondiente a la ruta actual
   const currentPath = window.location.pathname.split("/").pop();
@@ -28,4 +32,23 @@ import * as bootstrap from "bootstrap";
       link.classList.remove("active"); // Quita la clase de otros enlaces
     }
   });
+  //mapa de contacto - Leaflet
+  //https://leafletjs.com/download.html
+
+  //https://leafletjs.com/examples/custom-icons/
+  const customIcon = L.icon({
+    iconUrl: markerIcon, // Ruta al icono del marcador
+    shadowUrl: markerShadow, // Ruta a la sombra del marcador
+    iconSize: [55, 55], // Tamaño del icono
+    iconAnchor: [12, 41], // Punto del icono que se posiciona en el mapa
+    popupAnchor: [1, -34], // Punto desde el cual se abre el popup
+    shadowSize: [41, 41], // Tamaño de la sombra
+  });
+
+  const map = L.map("map").setView([41.54396363436662, 2.4422959973222857], 13);
+  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+  L.marker([41.54396363436662, 2.4422959973222857], { icon: customIcon })
+    .addTo(map)
+    .bindPopup("Jornadas de Economía Circular")
+    .openPopup();
 })();
